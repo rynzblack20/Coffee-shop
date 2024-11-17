169526a0c5c87772f80c996cf788b5a1fcde735f
rynzblack20 <ryanpurnama618@gmail.com>
2024-11-17 10:34:02 +0700

    Delete Coffee-Shop directory

--- Coffee-Shop/coffee_shop.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE menu_items (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    price INT NOT NULL,
-    description TEXT,
-    image VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
